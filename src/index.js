bc84304311098ade2637e8d4119edefb2fc17126
Tan MacPro <tanmacpro@Tans-MacBook-Pro.local>
2017-06-05 12:53:00 +0700

    initialized project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+import {load, save, remove} from './cookie-lib';
+import _ from 'lodash'
+
+let options = {
+  path: '/'
+}
+
+export function setOption(op = {}) {
+  options = _.merge(options, op)
+}
+
+export function setLocalItem(localStorageName, data) {
+  // SAVE TO COOKIE
+  save(localStorageName, JSON.stringify(data), options)
+}
+
+export function getLocalItem(localStorageName) {
+  // GET ITEM FROM LOCAL STORAGE
+  if (typeof document !== "undefined" && !!load(localStorageName)) {
+    return load(localStorageName)
+  } else {
+    return false;
+  }
+}
+
+export function removeLocalItem(localStorageName) {
+  // REMOVE ITEM FROM COOKIE
+  remove(localStorageName, options)
+}
+
+export function removeAllLocalItem(storageName) {
+  Object.keys(storageName).forEach(function(key) {
+    removeLocalItem(storageName[key])
+  });
+}
